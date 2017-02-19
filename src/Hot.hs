551c41a158246d7330cfdf372f1671cb087314ac
LFDM <1986gh@gmail.com>
2017-02-19 10:06:45 +0100

    Add more precise type info

--- src/Hot.hs
+++ src/Hot.hs
@@ -22,7 +22,9 @@ data FileStat = FileStat { path :: FilePath
                          } deriving (Show)
 
 type GitLineData = (String, String, String, [FileStat])
-type GitState = (String, GitLineData, [Commit])
+type State = String
+type GitState = (State, GitLineData, [Commit])
+type Line = String
 
 emptyLineData = ("", "", "", [])
 
@@ -55,29 +57,34 @@ flushState all@(s, d@(sha, _, _, _), res) =
   if null sha then all else ("BEGIN", emptyLineData, addRecord d)
    where addRecord lineData = (lineDataToCommit lineData):res
 
-processLines :: [String] -> GitState
+processLines :: [Line] -> GitState
 processLines = foldl processLine ("BEGIN", emptyLineData, [])
 
-processLine :: GitState -> String -> GitState
+processLine :: GitState -> Line -> GitState
 processLine x@("BEGIN", _, _) = processCommit x "AUTHOR"
 processLine x@("AUTHOR", _, _) = processAuthor x "DATE"
 processLine x@("DATE", _, _) = processDate x "STAT_BEGIN"
 processLine x@("STAT_BEGIN", _, _) = processStatBegin x "STAT"
 processLine x@("STAT", _, _) = processStat x "STAT"
 
+processCommit :: GitState -> State -> Line -> GitState
 processCommit a@(s, (_, author, date, fs), r) ns l =
   if null match then a else (ns, (match!!0!!1, author, date, fs), r)
   where match = l =~ "^commit (.+)" :: [[String]]
 
+processAuthor :: GitState -> State -> Line -> GitState
 processAuthor (s, (sha, _, date, fs), r) ns l = (ns, (sha, author, date, fs), r)
   where author = (l =~ "^Author: (.+)")!!0!!1
 
+processDate :: GitState -> State -> Line -> GitState
 processDate (s, (sha, author, _, fs), r) ns l = (ns, (sha, author, date, fs), r)
   where date = (l =~ "^Date: (.+)")!!0!!1
 
+processStatBegin :: GitState -> State -> Line -> GitState
 processStatBegin x ns l = if startsEmpty then x else processStat x ns l
   where startsEmpty = l =~ "^\\s" :: Bool
 
+processStat :: GitState -> State -> Line -> GitState
 processStat x ns l = if null match then flushState x else addStat x (match!!0)
   where match = l =~ "^([0-9]+)\\s+([0-9]+)\\s+(.+)" :: [[String]] -- \\d instead of [0-9] ain't working
         addStat (_, (sha, author, date, fs), r) (_:a:d:p:_) =
