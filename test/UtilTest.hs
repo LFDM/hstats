d670c45045ffbe19b09d21bdd3e3f6d21fcfcd35
LFDM <1986gh@gmail.com>
2017-03-04 18:57:19 +0100

    More tests

--- test/UtilTest.hs
+++ test/UtilTest.hs
@@ -24,4 +24,17 @@ suite = testGroup "Util"
     [ testCase "shortens a string to a given length I" $ (shorten 2 "12345") @=? "45"
     , testCase "shortens a string to a given length II" $ (shorten 8 "12345") @=? "12345"
     ]
+
+  , testGroup "addUnique"
+    [ testCase "adds element to a list when it's not already present" $ (addUnique 1 [2]) @=? [1, 2]
+    , testCase "doesn't add element when already present" $ (addUnique 1 [1]) @=? [1]
+    ]
+
+  , testGroup "removeLeading"
+    [ testCase "removes a leading string" $ (removeLeading "abc" "abcde") @=? "de"
+    ]
+
+  , testGroup "replaceLeading"
+    [ testCase "replaces leading elemnts in a list" $ (replaceLeading "abc" " " "abcde") @=? "   de"
+    ]
   ]
